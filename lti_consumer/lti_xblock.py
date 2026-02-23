fc1f2d3c0d60398af59e41ca68579556c31b7ee2
Navin Karkera <navin@disroot.org>
2026-02-23 14:10:55 +0530

    fix: quote resource_link_id properly as it is used in url by tools (#607)
    * fix: quote resource_link_id properly as it is used in url by tools
    
    * test: fix tests
    
    * chore: remove unnecessary casting

--- lti_consumer/lti_xblock.py
+++ lti_consumer/lti_xblock.py
@@ -1693,7 +1693,8 @@ class LtiConsumerXBlock(StudioEditableXBlockMixin, XBlock):
             user_id=self.lms_user_id,
             user_role=self._get_lti_1p3_user_role(),
             config_id=config_id,
-            resource_link_id=str(location),
+            # resource_link_id is used in the url params by the tool, so it should be url encoded.
+            resource_link_id=urllib.parse.quote(str(location)),
             external_user_id=self.external_user_id,
             preferred_username=username,
             name=full_name,
